f2dedd6d897c4c53ddf7d12d90788ee892a4a957
Tyler Nielsen <tylernielsenn@gmail.com>
2025-10-22 23:11:25 -0700

    I think it is subscribed now

--- radiacode/transports/bluetooth.py
+++ radiacode/transports/bluetooth.py
@@ -17,6 +17,33 @@ import bluetooth
 
 from radiacode.bytes_buffer import BytesBuffer
 
+from micropython import const
+
+_IRQ_CENTRAL_CONNECT = const(1)
+_IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_GATTS_WRITE = const(3)
+_IRQ_GATTS_READ_REQUEST = const(4)
+_IRQ_SCAN_RESULT = const(5)
+_IRQ_SCAN_DONE = const(6)
+_IRQ_PERIPHERAL_CONNECT = const(7)
+_IRQ_PERIPHERAL_DISCONNECT = const(8)
+_IRQ_GATTC_SERVICE_RESULT = const(9)
+_IRQ_GATTC_SERVICE_DONE = const(10)
+_IRQ_GATTC_CHARACTERISTIC_RESULT = const(11)
+_IRQ_GATTC_CHARACTERISTIC_DONE = const(12)
+_IRQ_GATTC_DESCRIPTOR_RESULT = const(13)
+_IRQ_GATTC_DESCRIPTOR_DONE = const(14)
+_IRQ_GATTC_READ_RESULT = const(15)
+_IRQ_GATTC_READ_DONE = const(16)
+_IRQ_GATTC_WRITE_DONE = const(17)
+_IRQ_GATTC_NOTIFY = const(18)
+_IRQ_GATTC_INDICATE = const(19)
+
+_ADV_IND = const(0x00)
+_ADV_DIRECT_IND = const(0x01)
+_ADV_SCAN_IND = const(0x02)
+_ADV_NONCONN_IND = const(0x03)
+
 class Bluetooth: 
 
     def __init__(self, mac, poll_interval: float = 0.01):
@@ -36,104 +63,126 @@ class Bluetooth:
         self._closing = False
         self._connection_state = None
 
-        PUBLIC = 0
-        # new shared variables
-        self.device = aioble.Device(PUBLIC, self.mac) 
-
-        asyncio.run(self.initialize_async())
-
-    async def initialize_async(self):
-        # connect to the peripheral 
-        while True:
-            try:
-                self.connection = await self.device.connect(timeout_ms=2000)
-                
-                break
-            except asyncio.TimeoutError:
-                print("Connection failed with Timeout")
-        print("Connection:", self.connection)
-
+        self._conn_handle = None
+        self._start_handle = None 
+        self._end_handle = None
+        self._ble = bluetooth.BLE()
+        self._ble.active(True) 
         # set up callbacks (notifications)
-        
-        # get the service by UUID - this is automatic client side?
-        # self.service = asyncio.run(self.connection.service(self._service_UUID))
-        self.service = None 
-        
-        while self.service == None:
-            services = await self._get_services()
-            print(services)
-            for s in services: 
-                if s.uuid == self._service_UUID: 
-                    self.service = s
+        self._ble.irq(self.handleNotification)
+
+        # connect to the peripheral
+        self.mac_bytes = bytes.fromhex(self.mac.replace(":",""))
+        self._addr_type = 0x00
+        self._addr = self.mac_bytes
+
+        self._conn_handle = None 
+        while self._conn_handle is None: 
+            self._ble.gap_connect(self._addr_type, self._addr)
+            start_time = time.ticks_ms()
+            while time.ticks_ms() - start_time < 2000: 
+                if self._conn_handle is not None: 
                     break
-            if self.service == None:
-                print("Failed to find service, retrying...")
-        print("Service:", self.service)
+                time.sleep_ms(100)
+        print("Connection:", self._conn_handle)
 
-        # get characteristics 
-        characteristics = await self._get_characteristics()
-        print("Characteristics:", characteristics)
-
-        # get the write_fd characteristics 
-        self.write_fd = characteristics[self._write_fd_UUID]
-        print("Write_fd", self.write_fd)
-
-        # get the notify_fd characteristics 
-        self.notify_fd = characteristics[self._write_fd_UUID]
-        print("Notify_fd", self.notify_fd)
-
-        # print("Notify_fd descriptors:", self._get_descriptors(self.notify_fd))
+        # get the service by UUID
+        # get characteristics
+        # discover services and UUID
+        # get the write_fd characteristics
+        # get the notify_fd characteristics
+        self._write_fd_handle = None
+        self._notify_fd_handle = None 
+        while (self._write_fd_handle is None) or (self._notify_fd_handle is None):
+            # search for any characteristics (this chains)
+            self._ble.gattc_discover_services(self._conn_handle, self._service_UUID)
+            start_time = time.ticks_ms()
+            while time.ticks_ms() - start_time < 2000: 
+                if (self._write_fd_handle is not None) and (self._notify_fd_handle is not None): 
+                    break
+                time.sleep_ms(100)
+            if (self._write_fd_handle is None) or (self._notify_fd_handle is None):
+                print("Finding services failed, trying again...")
 
         # write to the notify_fd characteristic (subscribe b'\x01\x00')
-        # await self.notify_fd.write(b'\x01\x00')
-        print("Props:", self.notify_fd.properties)
-        await self.notify_fd.subscribe(notify=True)
-        # asyncio.run(self.notify_fd.subscribe(notify=True))
-        print("Descriptors:", await self._get_descriptors(self.notify_fd))
+        print("Subscribing")
+        # self.p.writeCharacteristic(notify_fd + 1, b'\x01\x00')
+        self._notify_fd_cccd_handle = self._notify_fd_handle + 1
+        self.blocking_write(self._notify_fd_cccd_handle, b'\x01\x00')
 
-    async def _get_descriptors(self, target_char):
-        descriptors = []
-        async for d in target_char.descriptors(): 
-            descriptors.append(d)
-        return descriptors
+    def blocking_write(self, target_handle, data):
+        self._writing = True
+        self._ble.gattc_write(self._conn_handle, target_handle, data, 1)
+        while self._writing:
+            time.sleep_ms(100)
 
-    async def _get_characteristics(self):
-        characteristics = {}
-        async for c in self.service.characteristics():
-            characteristics[c.uuid] = c
-        return characteristics
+    def handleNotification(self, chandle, data): 
+        event = chandle 
+        print(f"Event! ({event})")
+        if event == _IRQ_PERIPHERAL_CONNECT:
+            conn_handle, addr_type, addr = data
+            if addr_type == self._addr_type and addr == self._addr: 
+                self._conn_handle = conn_handle
 
-    async def _get_services(self):
-        result = []
-        async for s in self.connection.services():
-            result.append(s)
-        return result
+        elif event == _IRQ_GATTC_SERVICE_RESULT:
+            # Connected device returned a service.
+            conn_handle, start_handle, end_handle, uuid = data
+            print("service", data)
+            if conn_handle == self._conn_handle and uuid == self._service_UUID:
+                self._start_handle, self._end_handle = start_handle, end_handle
 
-    def handleNotification(self, data): 
-        # check if this is a new message
-        if self._resp_size == 0:
-            # set up size 
-            self._resp_size = 4 + struct.unpack('<i', data[:4])[0]
-            self._resp_buffer = data[4:]
-        # copy data into response buffer 
-        else: 
-            self._resp_buffer += data
-        # reduce size
-        self._resp_size -= len(data)
-        # assert not really needed but helpful 
-        assert self._resp_size >= 0
-        # if we've received all of the message (size == 0)
-        if self._resp_size == 0:
-            # copy it over to _response 
-            self._response = self._resp_buffer
-            # reset _resp_buffer
-            self._resp_buffer = b''
+        elif event == _IRQ_GATTC_SERVICE_DONE:
+            # Service query complete.
+            if self._start_handle and self._end_handle:
+                self._ble.gattc_discover_characteristics(
+                    self._conn_handle, self._start_handle, self._end_handle
+                )
+            else:
+                print("Failed to find uart service.")
+
+        elif event == _IRQ_GATTC_CHARACTERISTIC_RESULT:
+            # Connected device returned a characteristic.
+            conn_handle, def_handle, value_handle, properties, uuid = data
+            if conn_handle == self._conn_handle and uuid == self._write_fd_UUID:
+                self._write_fd_handle = value_handle
+            if conn_handle == self._conn_handle and uuid == self._notify_fd_UUID:
+                self._notify_fd_handle = value_handle
+
+        elif event == _IRQ_GATTC_CHARACTERISTIC_DONE:
+            # Characteristic query complete.
+            if self._notify_fd_handle is not None and self._write_fd_handle is not None:
+                pass
+            else:
+                print("Failed to find characteristics.")
+
+        elif event == _IRQ_GATTC_WRITE_DONE:
+            conn_handle, value_handle, status = data
+            self._writing = False
+            print("Write done.")
+
+        elif event == _IRQ_GATTC_NOTIFY:
+            conn_handle, value_handle, notify_data = data
+            # check if this is a new message
+            if self._resp_size == 0:
+                # set up size 
+                self._resp_size = 4 + struct.unpack('<i', notify_data[:4])[0]
+                self._resp_buffer = notify_data[4:]
+            # copy data into response buffer 
+            else: 
+                self._resp_buffer += notify_data
+            # reduce size
+            self._resp_size -= len(notify_data)
+            # assert not really needed but helpful 
+            assert self._resp_size >= 0
+            # if we've received all of the message (size == 0)
+            if self._resp_size == 0:
+                # copy it over to _response 
+                self._response = self._resp_buffer
+                # reset _resp_buffer
+                self._resp_buffer = b''
 
     # synchronized, blocking send msg / receive response 
-    def execute(self, req) -> BytesBuffer:
-        return asyncio.run(self.execute_async(req))
-
-    async def execute_async(self, req) -> BytesBuffer: 
+    def execute(self, req) -> BytesBuffer: 
         # check for closing to be safe 
         if self._closing: 
             # raise error if so 
@@ -144,7 +193,8 @@ class Bluetooth:
         for pos in range(0, len(req), 18):
             # write characteristic to write_fd 
             rp = req[pos : min(pos + 18, len(req))]
-            await self.write_fd.write(rp)
+            # await self.write_fd.write(rp)
+            self.blocking_write(self._write_fd_handle, rp)
 
         # wait for response or timeout 
         timeout_end = time.ticks_ms() + (10 * 1000) # 10 s total timeout 
@@ -156,13 +206,13 @@ class Bluetooth:
             poll_time = min(self._poll_interval, remaining_time)
             
             # waitForNotifications
-            try: 
-                data = asyncio.wait_for(self.notify_fd.notified(), timeout=poll_time)
-                self.handleNotification(data)
-            except aioble.DeviceDisconnectedError as err:
-                raise ConnectionClosed('Bluetooth connection lost') from err
-            except asyncio.TimeoutError:
-                continue
+            # try: 
+            #     pass
+            #     # data = asyncio.wait_for(self.notify_fd.notified(), timeout=poll_time)
+            # except aioble.DeviceDisconnectedError as err:
+            #     raise ConnectionClosed('Bluetooth connection lost') from err
+            # except asyncio.TimeoutError:
+            #     continue
         if self._closing:
             raise ConnectionClosed('Connection closed while waiting for response')
 
@@ -178,7 +228,7 @@ class Bluetooth:
 
         # disconnect 
         try: 
-            asyncio.run(self.connection.disconnect())
+            self._ble.gap_disconnect(self._conn_handle)
         except: 
             pass
 
