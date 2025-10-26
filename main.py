8975411f2f09b1b7b765efe6c93c402f138f963d
Tyler Nielsen <tylernielsenn@gmail.com>
2025-10-26 11:59:37 -0700

    logging all incoming events and spectrums every 1 min

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import machine
 import time
+import os
+
+import sdcard
 
 from radiacode import RadiaCode
 from radiacode.transports.bluetooth import DeviceNotFound as DeviceNotFoundBT
-from radiacode import DoseRateDB
+from radiacode import DoseRateDB, RareData, RawData, RealTimeData, Event
 
 BLUETOOTH_MAC = "52:43:06:60:17:dd"
 SPECTRUM_DURATION_MS = (60 * 1000)
 
-print(f'Connecting to Radiacode via Bluetooth (MAC address: {BLUETOOTH_MAC})')
-
 while True: 
+  print("Setting up SD card...")
+  spi = machine.SPI()
+  spi.init()
+  sd = sdcard.SDCard(spi, machine.Pin.board.GP17)
+  print("Mounting filesystem...")
+  vfs = os.VfsFat(sd)
+  os.mount(vfs, "/fs")
+
+  it = 0
+  data_file_path = "" 
+  while True: 
+    try: 
+      data_file_path = f"/fs/data{it}.txt" 
+      f_info = os.stat(data_file_path)
+      it += 1
+    except OSError:
+      break # the name is unused
+
+  # create data file 
+  data_file = open(data_file_path, "w")
+
+  print(f"Created data file {data_file_path}")
+
+  print(f'Connecting to Radiacode via Bluetooth (MAC address: {BLUETOOTH_MAC})')
+
   try: 
     rc = RadiaCode(bluetooth_mac=BLUETOOTH_MAC)
   except DeviceNotFoundBT as e:
@@ -39,18 +65,31 @@ while True:
     start = time.ticks_ms() 
     while True: 
       for v in rc.data_buf(): 
-        print(v.dt.isoformat(), end=" ")
-        if type(v) == DoseRateDB:
-          print(v.count, v.count_rate, v.dose_rate, v.dose_rate_err, v.flags)
-        else: 
-          print(v)
+        print(v.dt.isoformat(), v)
+        t = type(v)
+        if t == DoseRateDB:
+          data_file.write(f"DoseRateDB; {v.dt}; {v.count}; {v.count_rate}; {v.dose_rate}; {v.dose_rate_err}; {v.flags};\n")
+        elif t == RareData: 
+          data_file.write(f"RareData; {v.dt}; {v.duration}; {v.dose}; {v.temperature}; {v.charge_level}; {v.flags};\n")
+        elif t == RealTimeData:
+          data_file.write(f"RealTimeData; {v.dt}; {v.count_rate}; {v.count_rate_err}; {v.dose_rate}; {v.dose_rate_err}; {v.flags}; {v.real_time_flags};\n")
+        elif t == RawData:
+          data_file.write(f"RawData; {v.dt}; {v.count_rate}; {v.dose_rate};\n")
+        elif t == Event: 
+          data_file.write(f"Event; {v.dt}; {v.event}; {v.event_param1}; {v.flags};\n")
       if time.ticks_ms() - start > SPECTRUM_DURATION_MS:
         start = time.ticks_ms()
         # read the spectrogram 
         spectrum = rc.spectrum()
-        print("Spectrum: ", spectrum.counts)
+        data_file.write(f"Spectrum; {spectrum.duration}; {spectrum.a0}; {spectrum.a1}; {spectrum.a2}; {spectrum.counts};")
+        print(f"{spectrum.duration} Spectrum: {spectrum}")
         # restart it 
         rc.spectrum_reset()
-        
+
   except Exception: 
+    # try to close data file if possible 
+    try: 
+      data_file.close()
+    except Exception:
+      pass
     continue 
